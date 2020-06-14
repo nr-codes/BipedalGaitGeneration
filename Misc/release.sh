a519c5ce31fe0220d3d67dc2c46437d2f75cf0e8
nr-codes <66048112+nr-codes@users.noreply.github.com>
2020-06-13 22:36:10 -0500

    working on workflow files

--- Misc/release.sh
+++ Misc/release.sh
@@ -10,29 +10,9 @@ ver="$1"
 #ver=$(git tag | tail -1)
 ver="${ver:1}"
 
-git tag -a $ver -m "new release"
-git push orign $ver
-
 # get script directory, https://stackoverflow.com/questions/59895
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd "$dir"
-
-# remove previous packages
-rm -f BipedalGaitGeneration*.zip GaitBrowser-*
-
-cd ../
-
-# create documentation
-echo "The content in this directory contains a video showing animations of" \
-     "all gaits presented in the figures of the paper as well as the source" \
-     "code for computing walking gaits from equilibria using numerical" \
-     "continuation methods.  Further information can be found in the" \
-     "README.txt file." > SUMMARY.txt 
-
-asciidoctor -a html_compact -a revnumber=v$ver README.adoc
-
-w3m -dump -o display_charset=latin1 README.html > README.txt
+cd "$dir/.."
 
 # set version in notebooks
 row='RowBox[{"VERSION", " ", "=", " ", "\\"\\<'
@@ -41,69 +21,10 @@ pat='[0-9]\+\.[0-9]\+\.[0-9]\+'
 reg="$row$pat$box"
 rep="$row$ver$box"
 sed -i "s/$reg/$rep/" Installer.nb
+
+# set version in package.json
 sed -i "s/\"version\": \"$pat\"/\"version\": \"$ver\"/" GaitBrowser/package.json
 
-# create downloadable archives
-math="BipedalGaitGeneration-Mathematica-$ver.zip"
-node="BipedalGaitGeneration-Node.js-$ver.zip"
-
-zip -9 $math \
-    README.txt \
-    SUMMARY.txt \
-    COPYING.txt \
-    Installer.nb \
-    RunModels.nb \
-    BipedalGaitGeneration.mp4 \
-    Figures \
-    Models \
-    SIMple \
-    GaitBrowser/src/bipeds \
-    GaitBrowser/app/imgs/*/*.png \
-    -x \*.gitignore
-
-zip -r9 $math \
-    Models \
-    SIMple \
-    -x Models/\*/\*CompiledFunctions 
-
-zip -r9 $node \
-    GaitBrowser \
-    -x GaitBrowser/exe\* \
-    -x GaitBrowser/node_modules\* \
-    -x GaitBrowser/app/imgs\* \
-    -x GaitBrowser/app/vids\* \
-    -x GaitBrowser/app/bin\* \
-    -x GaitBrowser/app/public/bipeds\* \
-    -x GaitBrowser/app/public/bundle.js \
-    -x GaitBrowser/package-lock.json
-
-cp $math ../TRO
-
-mv $math $node $dir
-
-rm README.html README.txt SUMMARY.txt
-
-# create executable web app
-cd ./GaitBrowser/
-
-rm -rf exe/
-npm i
-npm run build
-npm run pkg-all
-
-for f in exe/*
-do
-    [ -e "$f" ] || continue
-    base="${f##*/}-x64"
-    ext=""
-    if [[ "$f" == *.* ]]; then
-        ext=".${f##*.}"
-    fi
-
-    mv "$f" "$dir/$base-$ver$ext"
-done
-
-
-rm -rf exe/ node_modules/ package-lock.json app/bipeds app/bundle.js
-
-cd ../
+git commit -a
+git tag -a $ver -m "new release"
+git push orign $ver

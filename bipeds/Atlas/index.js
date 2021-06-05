867d2969f703bcccabdcedfb7631eed004e461d1
nr-codes <66048112+nr-codes@users.noreply.github.com>
2021-06-05 21:48:41 +0200

    adding static version of GaitBrowser (first pass, not all json files are working).

---
+++ bipeds/Atlas/index.js
@@ -0,0 +1,27 @@
+import { ColladaLoader } from '../../three/loaders/ColladaLoader.js';
+
+const path = './Atlas/meshes/';
+
+export default class AtlasColladaLoader extends ColladaLoader {
+  constructor(manager) {
+    super( manager );
+    this.setPath( path );
+  }
+
+  load( root, onLoad, onProgress, onError) {
+    const file = root.userData.json.mesh.file;
+
+    if(!file)  {
+      // console.log( `No mesh file present for ${root.name}.` );
+      if(onLoad) onLoad();
+    }
+    else {
+      const onload = (dae) => {
+        dae.scene.rotation.set( 0, 0, 0 );
+        if(onLoad) onLoad( dae.scene );
+      };
+      
+      super.load( file, onload, onProgress, onError );
+    }
+  }
+}
